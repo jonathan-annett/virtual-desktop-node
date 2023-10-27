8045a3781f75df053611cfba4f9a94c81d3c3a9b
jonathan-annett <jonathan.max.annett@gmail.com>
2023-10-27 17:41:30 +1100

    removed hash from url

--- index.js
+++ index.js
@@ -180,7 +180,7 @@ function switchToDesktop(n,cb) {
 
         const child = execFile(virtualDesktopExePath, [`/Switch:${n}` ], function (err,stdout,stderr) {
             if (err && err.code !== n) return cb (err);
-            cb(undefined,desktops.names [n]);
+            cb(undefined,desktops.names [n],n);
         }); 
         
     });
@@ -208,12 +208,12 @@ function nextDesktop(cb) {
     });
 }
  
-function desktopManager() {
+function desktopManager0() {
 
     let onchange;
     let self = {
         count,
-        goto : switchTo,
+        goto,
         names,
         visibleIndex,
         current,
@@ -242,10 +242,10 @@ function desktopManager() {
         }
     }
 
-    function switchTo (n) {
+    function goto (n) {
         return new Promise(function(resolve,reject){
-            switchToDesktop(n,function (err,name ){
-                return err ? reject(err) : resolve( name  );
+            switchToDesktop(n,function (err,name,index ){
+                return err ? reject(err) : resolve( {index,name}  );
             });
         });
             
@@ -266,6 +266,8 @@ function desktopManager() {
             });
         });
     }
+
+    
 
     function visibleIndex () {
         return new Promise(function(resolve,reject){
@@ -305,7 +307,241 @@ function desktopManager() {
         
     }
 
+
     
 }
 
 
+function desktopManager() {
+
+    var spawn = require('child_process').spawn;
+    var child = spawn(virtualDesktopExePath,['/INT']);
+
+    let data_in = '';
+
+    let onvariable;
+
+    const countResolves = [], namesResolves = [], objectCallbacks = [];
+ 
+    child.stdout.on('data', function(data) {
+       
+       data_in += data.toString();
+       const lines = data_in.split('\n');
+       data_in = '';
+      
+       while (lines.length>0) {
+
+         const line = lines.shift();
+         
+         if (line.startsWith('{') ) {
+
+            if (line.endsWith('}')) {
+                
+                // process json object line
+                try {
+                    processObject(JSON.parse(line));
+                } catch (err) {
+                    console.log(err)
+                }
+
+            } else {
+
+                if (lines.length===0) {
+                    // this is an incomplete json object line, save it for later
+                    data_in = line;
+                    break;
+                }
+
+            }
+
+         } else {
+            if (line.startsWith('[') ) {
+                if (line.endsWith(']') ) {
+                    try {
+                        processArray(JSON.parse(line));
+                    } catch (err) {
+                        console.log(err)
+                    }
+                } else {
+                    if (lines.length===0) {
+                         // this is an incomplete json array line, save it for later
+                        data_in = line;
+                        break;
+                    }
+                }
+            } else {
+               // ignore non JSON line
+            }
+         }
+       }
+
+    });
+
+    const self = {
+
+        count,
+        goto,
+        next,
+        previous,
+        current,
+        names,
+        send,
+        visibleIndex,
+        on: function(e,fn) {
+            switch (e) {
+                case 'variable' : onvariable = fn; break;
+                case 'change' : self.onchange = fn; break;
+                    
+            }
+        }
+    };
+
+    let onchange,last;
+
+    Object.defineProperties(self,{onchange:{
+        get : function () { return onchange;},
+        set : function(v) {
+          
+            if (typeof v==='function') {
+                onchange = v;
+            }
+        },
+        enumerable: true,
+    }});
+
+
+    return self;
+
+    function processObject(obj) {
+        console.log({obj});
+
+
+        if (typeof onchange + typeof obj.visibleIndex + typeof obj.visible === 'functionnumberstring' ) {
+            const token = `${obj.visibleIndex}:${obj.visible}`;
+            if (last !== token) {
+                last=token;
+                onchange(obj);
+            }
+        }
+
+        objectCallbacks.forEach(function(fn){fn(obj);});
+
+        if (typeof onvariable==='function') {
+
+            if (typeof obj.visibleIndex==='number') {
+                onvariable('visibleIndex',obj.visibleIndex);
+            }
+            if (typeof obj.visible === 'string') {
+                onvariable('visible',obj.visible);
+            }
+        }
+        
+    }
+
+    function processArray(arr) {
+        console.log({arr});
+
+        countResolves.splice(0,countResolves.length).forEach(function(resolve){
+            resolve(arr.length);
+        });
+
+        namesResolves.splice(0,namesResolves.length).forEach(function(resolve){
+            resolve(arr);
+        });
+
+        if (typeof onvariable==='function') {
+            onvariable('count',arr.length);
+            arr.forEach(function(nm,i){
+                onvariable(`Desktop-${i+1}`,nm);
+            });
+        }
+    }
+
+    function send (cmd){
+        child.stdin.write(`${cmd}\n`);
+        
+    }
+
+    function visibleIndex () {
+        return new Promise(function(resolve,reject) {
+            objectCallbacks.push(onObj);
+
+            send('gcd');
+
+            function onObj(obj) {
+                objectCallbacks.splice(objectCallbacks.indexOf(onObj),1);
+                resolve(obj);
+            }
+        });
+
+    }
+
+
+    function names() {
+        return new Promise(function(resolve,reject) {
+            namesResolves.push(resolve);
+            send ('names');      
+        });
+    }
+
+    function goto (dt) {
+        return new Promise(function(resolve) {
+
+            objectCallbacks.push(onObj);
+            send(`s:${dt}`);
+
+            function onObj(obj) {
+                if( (typeof dt==='string' && obj.visible === dt)||
+                    (typeof dt==='number' && obj.visibleIndex === dt) ) {
+                    objectCallbacks.splice(objectCallbacks.indexOf(onObj),1);
+                    resolve(obj);
+                }
+            }
+        });
+    }
+
+    function previous (index) {
+
+        return new Promise(function(resolve,reject) {
+            objectCallbacks.push(onObj);
+            send ('left');     
+            function onObj(obj) {
+                objectCallbacks.splice(objectCallbacks.indexOf(onObj),1);
+                resolve(obj);
+            }       
+        });
+    }
+
+    function next (index) {
+        return new Promise(function(resolve,reject) {
+            objectCallbacks.push(onObj);
+            send ('right');     
+            function onObj(obj) {
+                objectCallbacks.splice(objectCallbacks.indexOf(onObj),1);
+                resolve(obj);
+            }       
+        });
+    }
+
+    function current () {
+        return new Promise(function(resolve,reject) {
+            objectCallbacks.push(onObj);
+            send ('gcd');     
+            function onObj(obj) {
+                objectCallbacks.splice(objectCallbacks.indexOf(onObj),1);
+                resolve(obj);
+            }       
+        });
+    }
+
+
+    function count () {
+        return new Promise(function(resolve,reject) {
+            countResolves.push(resolve);
+            send ('names');
+            
+        });
+    }
+
+ 
+}
